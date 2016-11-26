369cdfc6c81a414c8e201149a6f9364b6c5c3fbd
sajjadzamani <szamani@uwaterloo.ca>
2016-11-25 21:26:38 -0500

    fixed ball.ino

--- main/Ball.ino
+++ main/Ball.ino
@@ -1,72 +1,28 @@
-float Vx = 0.0;
-float Vy = 0.0;
-float xPos = 0.0;
-float yPos = 0.0;
+#include <delay.h>
+#include <FillPat.h>
+#include <LaunchPad.h>
+#include <OrbitBoosterPackDefs.h>
+#include <OrbitOled.h>
+#include <OrbitOledChar.h>
+#include <OrbitOledGrph.h>
 #include <math.h>
 
-<<<<<<< .mine
-||||||| .r2325
-//float Ax,Ay;
-float Vx_0=0,Vy_0=0;
-float Vx;
-float Vy;
-float xPos,x_0=0;
-float yPos , y_0=0;
-
-=======
 #define FRAME_TIME 0.04f
 #define REBOUND_FACTR 0.2f
 #define MAX_MARBLE_ACC 50
 
->>>>>>> .r2523
-<<<<<<< .mine
-void GetSpeed(){
-  Vx += -1.0 * Ax / 9.8 * 32;
-  Vy += 1.0 * Ay / 9.8 * 32;
- Serial.print("Vx: ");
- Serial.print(Vx);
- Serial.print(" ");
- Serial.print("Vy:");
- Serial.println(Vy);
- }
-||||||| .r2325
-void GetSpeed(){
-  Vx=Ax*(1000.0/1000.0)+Vx_0;
-  Vy=Ay*(1000.0/1000.0)+Vy_0;
-  Vx_0=Vx;
-  Vy_0=Vy;
- Serial.print("x:");
- Serial.print(Vx);
- Serial.print(" ");
- Serial.print("y:");
- Serial.println(Vy);
- }
-=======
 struct speed{
 float x;
 float y;
 };
->>>>>>> .r2523
 
-<<<<<<< .mine
-void GetPos(){
-  GetSpeed();
-  xPos += Vx * (PERIOD_S);
-  yPos += Vy * (PERIOD_S);
-  Serial.print("Xpos: ");
-||||||| .r2325
-void GetPos(){
-  GetSpeed();
-  xPos=Vx*(10.0/1000.0)+x_0;
-  yPos=Vy*(10.0/1000.0)+y_0;
-  Serial.print("Xpos");
-=======
 struct position{
   int x;
   int y;
 };
 
 struct position curPosition = (struct position){.x = 1, .y = 1};
+
 struct speed curSpeed = (struct speed){0};
 extern char    rgbOledBmp[];
 
@@ -121,32 +77,12 @@ struct position GetPosition(){
    Serial.print("\n");*/
   return curPosition;
  /*Serial.print("Xpos");
->>>>>>> .r2523
   Serial.println(xPos);
-  Serial.print("Ypos: ");
-<<<<<<< .mine
-  Serial.println(yPos);
-||||||| .r2325
-  Serial.println(yPos);
-   
- 
-  x_0=xPos;
-  y_0=yPos;
-=======
+  Serial.print("ypos");
   Serial.println(yPos);*/
   
->>>>>>> .r2523
 }
 void DrawBall(){
-<<<<<<< .mine
-  GetPos();
-  OrbitOledClearBuffer();
-  OrbitOledMoveTo(xPos,yPos);
-  OrbitOledDrawPixel();
-||||||| .r2325
-  GetPos();
-  OrbitOledMoveTo(-xPos,-yPos);
-=======
   OrbitOledClearBuffer();
   for(int i=curPosition.x;i<=(curPosition.x+3);i++){
     if(i==curPosition.x || i==curPosition.x+3){
@@ -175,31 +111,14 @@ void ClearBall(){
   OrbitOledFillRect(curPosition.x+2,curPosition.y+3);
   OrbitOledMoveTo(curPosition.x,curPosition.y+1);
   OrbitOledFillRect(curPosition.x+3,curPosition.y+2);
->>>>>>> .r2523
   OrbitOledUpdate();
-<<<<<<< .mine
-  //pbOledCur+=xPos;
-  //OrbitOledDrawPixel()
-}
-||||||| .r2325
-  OrbitOledPutChar('o');
-  
-  
-  
-  //pbOledCur+=xPos;
-  //OrbitOledDrawPixel()
-}
-=======
 }
 
 void MoveBall(){
+  ClearBall();
   Move();
   GetSpeed();
   GetPosition();
-  ClearBall();
   DrawBall();
-  
- 
 }
 
->>>>>>> .r2523
